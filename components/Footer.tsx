ebda430a8a37bbf6f3a8d9c483af78ffb13e0553
Bruno Pistarino <brunopistarino@gmail.com>
2023-08-04 22:18:07 -0300

    a

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,15 +2,17 @@ import { Facebook, Instagram, Twitter } from "lucide-react";
 
 export default function Footer() {
   return (
-    <footer className="pt-16 pb-12 flex flex-col gap-16 bg-slate-950 items-center text-white">
-      <div className="max-w-7xl px-8 flex justify-between gap-8 w-full">
-        <div className="flex flex-col gap-8">
-          <div className="flex items-center gap-4">
-            <img src="appicon.png" className="h-12" alt="" />
-            <p className="font-semibold text-2xl">InClass</p>
+    <footer className="pt-12 md:pt-16 pb-12 flex flex-col gap-12 md:gap-16 bg-slate-950 items-center text-white">
+      <div className="max-w-7xl px-8 flex flex-col md:flex-row justify-between gap-8 w-full items-start">
+        <div className="flex flex-col gap-8 w-full">
+          <div className="flex flex-col gap-6 md:gap-8">
+            <div className="flex items-center gap-4">
+              <img src="appicon.png" className="h-12" alt="" />
+              <p className="font-semibold text-2xl">InClass</p>
+            </div>
+            <p>Banking technology that has your back.</p>
           </div>
-          <p>Banking technology that has your back.</p>
-          <div className="flex gap-8">
+          <div className="md:flex gap-3 md:gap-8 grid grid-cols-2">
             <p>Overview</p>
             <p>Features</p>
             <p>Pricing</p>
@@ -29,9 +31,9 @@ export default function Footer() {
         </div>
       </div>
       <div className="max-w-7xl px-8 w-full">
-        <div className=" flex pt-8 justify-between text-slate-400 border-t border-slate-400">
+        <div className=" flex pt-8 justify-between text-slate-400 border-t border-slate-400 flex-col sm:flex-row gap-6 md:gap-8">
           <p>© {new Date().getFullYear()} InClass. All rights reserved.</p>
-          <div className="flex gap-6">
+          <div className="flex gap-6 justify-center">
             <Instagram />
             <Facebook />
             <Twitter />
