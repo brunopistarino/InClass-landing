d1cfa4574cce395c35071973d79a5c746543944c
Bruno Pistarino <brunopistarino@gmail.com>
2023-08-29 16:25:38 -0300

    a

--- .contentlayer/generated/Doc/_index.mjs
+++ .contentlayer/generated/Doc/_index.mjs
@@ -3,8 +3,9 @@
 import blog__deployingNextAppsMdx from './blog__deploying-next-apps.mdx.json' assert { type: 'json' }
 import blog__mdMdx from './blog__md.mdx.json' assert { type: 'json' }
 import blog__privacyPolicyMdx from './blog__privacy-policy.mdx.json' assert { type: 'json' }
+import blog__termsAndConditionsOldMdx from './blog__terms-and-conditions-old.mdx.json' assert { type: 'json' }
 import blog__termsAndConditionsMdx from './blog__terms-and-conditions.mdx.json' assert { type: 'json' }
 import blog__testMdx from './blog__test.mdx.json' assert { type: 'json' }
 import blog__usageMdx from './blog__usage.mdx.json' assert { type: 'json' }
 
-export const allDocs = [blog__deployingNextAppsMdx, blog__mdMdx, blog__privacyPolicyMdx, blog__termsAndConditionsMdx, blog__testMdx, blog__usageMdx]
+export const allDocs = [blog__deployingNextAppsMdx, blog__mdMdx, blog__privacyPolicyMdx, blog__termsAndConditionsOldMdx, blog__termsAndConditionsMdx, blog__testMdx, blog__usageMdx]
